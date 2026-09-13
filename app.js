20ba81e6af43d1abd2369d6d848447fced474241
Rajaryan99 <raj.aryan2518@gmail.com>
2026-09-14 01:29:35 +0530

    used multer middleware to send file in req

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 import express from 'express'
 import 'dotenv/config'
+import multer from 'multer';
+
+const upload = multer({dest: 'uploads/'})
 
 const app = express()
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
 	res.send("hello world")
+})
+
+app.post('/upload', upload.single('pdf'), (req, res) => {
+    console.log(req.file)
+    res.send("file uploaded successfully")
+
 })
 
 app.listen(port, (req, res) => {
