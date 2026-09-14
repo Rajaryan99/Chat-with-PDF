7d52e27c754d3fd465cbdaa10b4a25142726fb4e
Rajaryan99 <raj.aryan2518@gmail.com>
2026-09-14 14:32:09 +0530

    added a API endpoint /uploade to upload Pdf

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import 'dotenv/config'
 import multer from 'multer';
+import fs from 'node:fs/promises'
+import {PDFParse} from 'pdf-parse'
 
 const upload = multer({dest: 'uploads/'})
 
@@ -11,11 +13,25 @@ app.get('/', (req, res) => {
 	res.send("hello world")
 })
 
-app.post('/upload', upload.single('pdf'), (req, res) => {
-    console.log(req.file)
-    res.send("file uploaded successfully")
-
+app.post('/upload', upload.single('pdf'), async (req, res) => {
+    
+try {
+     console.log(req.file)
+    const dataBuffer = await fs.readFile(req.file.path);
+    const pdfData = new PDFParse({data: dataBuffer})
+    const pdfText = await pdfData.getText()
+    console.log(pdfText.text)
+     res.send(["file uploaded successfully",pdfText.text] )
+    
+} catch (error) {
+    console.error(error)
+    res.status(500).send("Error reading file")
+    
+}
+   
 })
+
+
 
 app.listen(port, (req, res) => {
 	console.log(`server is running on http://localhost:${port}`)
