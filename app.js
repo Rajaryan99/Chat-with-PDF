345fd62fdaa23a6c20303fd24abac10aba552dc8
Rajaryan99 <raj.aryan2518@gmail.com>
2026-09-15 23:08:47 +0530

    created an embadding model to convert the chunks into vector

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ const ai = new GoogleGenAI({
     apiKey: process.env.GEMINI_API_KEY
 })
 
+async function createEmbadding(text) {
+    const response = await ai.models.embedContent({
+        model: 'gemini-embedding-2',
+        contents: text,
+    });
+
+    return response.embeddings
+}
 
 const upload = multer({dest: 'uploads/'})
 
@@ -26,25 +34,30 @@ app.get('/', (req, res) => {
 app.post('/upload', upload.single('pdf'), async (req, res) => {
     
 try {
-     console.log(req.file)
+     console.log(req.body)
     const dataBuffer = await fs.readFile(req.file.path);
     const pdfData = new PDFParse({data: dataBuffer})
     const pdfText = await pdfData.getText()
     const text = pdfText.text
 
-    const chunks = text.split('\n\n')
-    console.log(chunks)
+    const chunks = text.split('\n\n').filter((chunk) => chunk.trim() != "" )
+    // console.log(chunks)
 
-
+    const embaddings  = await createEmbadding(chunks[0])
+    console.log(embaddings)
 
     // for(let i=0; i<=text.length; i+=500){
     //     chunks.push(text.slice(i, i+500))
     // }
 
+    const question = req.body.question
+    const matchChunk = chunks.find((chunk) => chunk.toLocaleLowerCase().includes('raj aryan'))
+    console.log("Matching chunks: ", matchChunk)
+
 
     const response = await ai.models.generateContent({
         model: 'gemini-3.6-flash',
-        contents: `Explain this PDF in plan and easy way ${chunks[0]}`
+        contents: `Answer the question using context ${matchChunk} and question is ${question}`
     })
 
     res.status(200).send(response.text)
